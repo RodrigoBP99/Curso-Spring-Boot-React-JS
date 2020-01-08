1073f23c7a253b11f7c76a5dfbc01cbfca55c7a1
RodrigoBP99 <rodrigobozapontes@gmail.com>
2020-01-08 17:53:25 -0300

    Corrige erros em metodos

--- src/main/java/com/example/minhasFinancas/model/repository/LancamentoRepository.java
+++ src/main/java/com/example/minhasFinancas/model/repository/LancamentoRepository.java
@@ -6,14 +6,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import com.example.minhasFiancas.model.enums.StatusLancamento;
 import com.example.minhasFiancas.model.enums.TipoLancamento;
 import com.example.minhasFinancas.model.entity.Lancamento;
 
 public interface LancamentoRepository extends JpaRepository<Lancamento, Long> {
 	
 	@Query( value = "select sum(l.valor) from Lancamento l join l.usuario u "
-			+ "where u.id = :idUsuario and l.tipo = :tipo group by u")
-	public BigDecimal obterSaldoPorTipoLancamentoEUsuario(
+			+ "where u.id = :idUsuario and l.tipo = :tipo and l.status = :status group by u")
+	public BigDecimal obterSaldoPorTipoLancamentoEUsuarioEStatus(
 			@Param("idUsuario") Long idUsuario,
-			@Param("tipo") TipoLancamento tipo);
+			@Param("tipo") TipoLancamento tipo,
+			@Param("status") StatusLancamento status);
+	
 }
